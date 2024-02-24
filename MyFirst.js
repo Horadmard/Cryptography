1eba1c8237c4d0288897de6d3598ac07f71016a9
Horadmard <iranradmard1@gmail.com>
2024-02-25 02:15:58 +0330

    Edit

--- MyFirst.js
+++ MyFirst.js
@@ -1,8 +1,9 @@
+var http = require('http');
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello World!');
-}).listen(8080);
+  }).listen(8080);
 
-exports.myDateTime = function () {
-    return Date();
-};
+// exports.myDateTime = function () {
+//     return Date();
+// };
