089ea02b5b15107e46800752ac7e7fc6171c6e3d
Horadmard <iranradmard1@gmail.com>
2024-02-25 02:23:21 +0330

    Test

--- MyFirst.js
+++ MyFirst.js
@@ -1,10 +1,13 @@
 var http = require('http');
+var fs = require('fs');
 
-//create a server object:
 http.createServer(function (req, res) {
-    res.write('Hello World!'); //write a response to the client
-    res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+    fs.readFile('demofile1.html', function (err, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+    });
+}).listen(8080);
 
 exports.myDateTime = function () {
     return Date();
