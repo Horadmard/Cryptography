e8d0a6223e771175ec4180253086f40ffc1be143
Horadmard <iranradmard1@gmail.com>
2024-02-25 02:18:31 +0330

    Edit

--- MyFirst.js
+++ MyFirst.js
@@ -1,8 +1,10 @@
 var http = require('http');
+
+//create a server object:
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
-  }).listen(8080);
+    res.write('Hello World!'); //write a response to the client
+    res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
 
 exports.myDateTime = function () {
     return Date();
