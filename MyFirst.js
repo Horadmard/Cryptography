a6218e7721c8337b7a4c3ed9393f81659c314ce1
Horadmard <iranradmard1@gmail.com>
2024-02-25 02:23:35 +0330

    Test

--- MyFirst.js
+++ MyFirst.js
@@ -1,13 +1,13 @@
-var http = require('http');
-var fs = require('fs');
+var url = require('url');
+var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+var q = url.parse(adr, true);
 
-http.createServer(function (req, res) {
-    fs.readFile('demofile1.html', function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-    });
-}).listen(8080);
+console.log(q.host); //returns 'localhost:8080'
+console.log(q.pathname); //returns '/default.htm'
+console.log(q.search); //returns '?year=2017&month=february'
+
+var qdata = q.query; //returns an object: { year: 2017, month: 'february' }
+console.log(qdata.month); //returns 'february'
 
 exports.myDateTime = function () {
     return Date();
