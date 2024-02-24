e45b36d679cee43fa43214df2b13523be6faae01
Hossein Radmard <124653035+Horadmard@users.noreply.github.com>
2024-02-25 01:47:40 +0330

    add some codes

--- MyFirst.js
+++ MyFirst.js
@@ -1,4 +1,8 @@
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end('Hello World!');
-  }).listen(8080);
+}).listen(8080);
+
+exports.myDateTime = function () {
+    return Date();
+};
