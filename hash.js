c3f7b3570fbf2afd63a88b29f159b66e4fc6b994
Horadmard <iranradmard1@gmail.com>
2024-02-26 02:47:24 +0330

    Update

--- hash.js
+++ hash.js
@@ -3,3 +3,7 @@ const { createHash } = require('crypto');
 function hash(input){
     return createHash('sha256').update(input).digest('hex');
 }
+
+let password = 'Hey-papa!';
+const hash1 = hash(password);
+console.log(hash1);
