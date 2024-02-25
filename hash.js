81ee5f51b1b36f7b4640fa870a1e00474a3e9508
Horadmard <iranradmard1@gmail.com>
2024-02-26 03:09:57 +0330

    Update Hmac

--- hash.js
+++ hash.js
@@ -1,20 +1,20 @@
 
 /// ------------------------------------------- Hash function
-const { createHash, timingSafeEqual } = require('crypto');
+// const { createHash, timingSafeEqual } = require('crypto');
 
-function hash(input) {
-    return createHash('sha256').update(input).digest('hex');
-}
+// function hash(input) {
+//     return createHash('sha256').update(input).digest('hex');
+// }
 
-let password = 'Hey-papa!';
-const hash1 = hash(password);
-console.log(hash1);
+// let password = 'Hey-papa!';
+// const hash1 = hash(password);
+// console.log(hash1);
 
-password = 'Hey-papa!';
-const hash2 = hash(password);
-console.log(hash2);
+// password = 'Hey-papa!';
+// const hash2 = hash(password);
+// console.log(hash2);
 
-console.log(hash1 == hash2 ? 'good password' : 'password does not match');
+// console.log(hash1 == hash2 ? 'good password' : 'password does not match');
 
 /// ------------------------------------------- Salt
 
