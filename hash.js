c36fc9b6bac99192dfd7ff79b75cc12ed61b7ca9
Horadmard <iranradmard1@gmail.com>
2024-02-26 02:59:56 +0330

    Update salt

--- hash.js
+++ hash.js
@@ -8,10 +8,31 @@ let password = 'Hey-papa!';
 const hash1 = hash(password);
 console.log(hash1);
 
-///--------------------------
+/// Hash function
 
 password = 'Hey-papa!';
 const hash2 = hash(password);
 console.log(hash2);
 
 console.log(hash1 == hash2 ? 'good password' : 'password does not match');
+
+/// Salt
+
+const { scryptSync, randomBytes } = require('crypto');
+
+function signup(email, password) {
+    const salt = randomBytes(16).toString('hex');
+    const hashedpassword = scryptSync(password, salt, 64);
+
+    const user = { email, password: `${salt}:${hashedpassword}` }
+    users.push(user);
+    return user
+}
+
+function login(email, password) {
+    const user = users.find(v => v.email === email);
+
+    cont[salt, key] = user.password.split(':');
+
+}
+
