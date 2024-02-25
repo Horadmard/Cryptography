bfce30e0fe226952644a0ff0db0a1a7d5103f113
Horadmard <iranradmard1@gmail.com>
2024-02-26 03:05:12 +0330

    Update salt

--- hash.js
+++ hash.js
@@ -1,4 +1,6 @@
-const { createHash } = require('crypto');
+
+/// ------------------------------------------- Hash function
+const { createHash, timingSafeEqual } = require('crypto');
 
 function hash(input) {
     return createHash('sha256').update(input).digest('hex');
@@ -8,15 +10,13 @@ let password = 'Hey-papa!';
 const hash1 = hash(password);
 console.log(hash1);
 
-/// Hash function
-
 password = 'Hey-papa!';
 const hash2 = hash(password);
 console.log(hash2);
 
 console.log(hash1 == hash2 ? 'good password' : 'password does not match');
 
-/// Salt
+/// ------------------------------------------- Salt
 
 const { scryptSync, randomBytes } = require('crypto');
 const { buffer } = require('stream/consumers');
@@ -37,6 +37,13 @@ function login(email, password) {
     const hashedBuffer = scryptSync(password, salt, 64);
 
     const keyBuffer = Buffer.form(key, 'hex');
+    const match = timingSafeEqual(hashedBuffer, keyBuffer);
+
+    if (match) {
+        return 'login success!'
+    } else {
+        return 'login failed!'
+    }
 
 }
 
