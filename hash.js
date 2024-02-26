3e0ada65fc64229da6bb8eaf9ce7f312b09f634d
Horadmard <iranradmard1@gmail.com>
2024-02-26 03:46:32 +0330

    Hmac.js

--- hash.js
+++ hash.js
@@ -18,19 +18,7 @@
 
 
 
-/// ------------------------------------------- Hmac
-// const {createHmac} = require('crypto')
-// const key = 'super-secret!';
-// const massage = 'boo!'
 
-// const hmac = createHmac('sha256', key).update(massage).digest('hex');
-
-// console.log(hmac);
-
-// const key2 = 'other-password';
-// const hmac2 = createHmac('sha256', key2).update(massage).digest('hex');
-
-// console.log(hmac2);
 
 /// ------------------------------------------- Ecnryption using aes256
 
